946563217d614cf4368c90340d1e421d1d828348
rmcarpenter <48769319+rmcarpenter@users.noreply.github.com>
2019-03-27 14:15:47 -0500

    Add files via upload

---
+++ Foodinspections.1.19.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(lubridate)
+
+
+fdinsp=read.csv("Food_Inspections (4).csv")
+
+insp.dates=as.POSIXct(fdinsp$Inspection.Date, format = '%m/%d/%Y')
+
+fdinsp=cbind(insp.dates,fdinsp)
+
+#January 
+jan.insp.fail = fdinsp %>% 
+  filter(insp.dates >= as.Date("2019-02-01") & insp.dates <= as.Date("2019-02-22")) %>% 
+  filter(Results=="Fail")
+
+
+
+
+
+leaflet(data = jan.insp.fail %>% addTiles() %>%
+  addMarkers(Longitude, Latitude)) 
+
+leaflet(jan.insp.fail) %>% addTiles() %>% addCircleMarkers()
+
+leaflet(data = jan.insp.fail) %>% addTiles%>%
+  addMarkers(popup  = ~htmlEscape(AKA.Name))
+
+
+d.in=read.delim("Compressed Mortality, 1999-2016 (1).txt")
+
